9e8a423e41a06a97d30b0e380968e5b50742a583
Danilo <danilodiniz93@yahoo.com>
2024-07-04 15:15:48 -0300

    project started

---
+++ src/main/java/com/danilodinizs/springboot/models/ProductModel.java
@@ -0,0 +1,26 @@
+package com.danilodinizs.springboot.models;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel implements Serializable{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    UUID idProduct;
+    String name;
+    BigDecimal value;
+
+}
