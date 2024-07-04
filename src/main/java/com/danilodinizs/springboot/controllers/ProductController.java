1bd87aa40d725adb6143f5060afd3aa8c01b661d
Danilo <danilodiniz93@yahoo.com>
2024-07-04 15:54:15 -0300

    created ProductController

--- src/main/java/com/danilodinizs/springboot/controllers/ProductController.java
+++ src/main/java/com/danilodinizs/springboot/controllers/ProductController.java
@@ -1,4 +1,13 @@
 package com.danilodinizs.springboot.controllers;
 
+import com.danilodinizs.springboot.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+    @Autowired
+    ProductRepository productRepository;
+
+
 }
---
+++ src/main/java/com/danilodinizs/springboot/dtos/ProductRecordDto.java
@@ -0,0 +1,4 @@
+package com.danilodinizs.springboot.dtos;
+
+public record ProductRecordDto() {
+}
